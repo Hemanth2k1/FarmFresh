a2b8ed4882c7d85daa94f557c2bddfe48371ebeb
Hemanth Kalluri <hemanthkalluri@Hemanths-MacBook-Air.local>
2025-05-15 12:20:17 -0500

    Initial commit

---
+++ src/main/java/com/org/SpringBoot/AppController.java
@@ -0,0 +1,87 @@
+package com.org.SpringBoot;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+   
+@Controller
+public class AppController {
+ 
+	 private static final Logger logger = LoggerFactory.getLogger("AppController.class");
+    @Autowired
+    private UserRepository userRepo;
+     
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
+    }
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+         
+        return "signup_form";
+    } 
+    @GetMapping("/login")
+    public String login() {
+         
+        return "/login";
+    } 
+    
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+         
+        userRepo.save(user);
+         
+        return "register_success";
+    }    
+    
+    @GetMapping("/home1")
+    public String home() {
+    	return "home1";
+    }
+
+    @GetMapping("/users")
+    public String listUsers(Model model) {
+        List<User> listUsers = userRepo.findAll();
+        model.addAttribute("listUsers", listUsers);
+         
+        return "users";
+    }
+
+}
